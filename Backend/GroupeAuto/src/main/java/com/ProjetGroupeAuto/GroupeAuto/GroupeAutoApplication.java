304fee21e0a31c82f5b3a60fe0238655025dd394
Modibo Sangare <sangaremodibo57@gmail.com>
2021-12-17 20:08:17 +0000

    Créeation du Backend

---
+++ Backend/GroupeAuto/src/main/java/com/ProjetGroupeAuto/GroupeAuto/GroupeAutoApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetGroupeAuto.GroupeAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupeAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupeAutoApplication.class, args);
+	}
+
+}
---
+++ Backend/GroupeAuto/src/test/java/com/ProjetGroupeAuto/GroupeAuto/GroupeAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetGroupeAuto.GroupeAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupeAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
