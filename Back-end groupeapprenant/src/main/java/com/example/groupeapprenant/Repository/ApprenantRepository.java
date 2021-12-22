18112a95e37f8299e28ca9aa6fde405bfddf683d
Souleymane <soulnew123@gmail.com>
2021-12-22 17:54:56 +0000

    mise à jour maquette

--- Back-end groupeapprenant/src/main/java/com/example/groupeapprenant/Repository/ApprenantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.groupeapprenant.Repository;
-
-import com.example.groupeapprenant.Model.Apprenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApprenantRepository extends JpaRepository<Apprenant, Long> {
-}
